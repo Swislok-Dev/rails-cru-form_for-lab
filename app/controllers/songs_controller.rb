e2d5028e297d8ef352acf33b8b2f319a3203035d
Swislok-Dev <cmarker4@gmail.com>
2021-03-15 16:09:48 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,14 +3,12 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
-  def new
-    @song = Song.new
-  end
-
   def show 
     @song = Song.find(params[:id])
-    @genre = Genre.find(params[:id])
-    @artist = Artist.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
   end
 
   def edit
@@ -18,19 +16,19 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.create(song_params)
+    @song = Song.create(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
   def update
     @song = Song.find(params[:id])
-    @song.update(song_params)
+    @song.update(song_params(:name, :artist_id, :genre_id))
     redirect_to song_path(@song)
   end
 
   private
 
-  def song_params
-    params.require(:song)
+  def song_params(*args)
+    params.require(:song).permit(*args)
   end
 end
